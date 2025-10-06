3c8d0f123c218d41257be210dea621a97365862f
aawiegel <aawiegel>
2025-10-06 22:12:17 +0000

    add notebook

---
+++ src/data_generation/generate_sample_data.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Sample data generation
+# MAGIC
+# MAGIC This notebook is used to randomly generate sample data for demonstration purposes.
+
+# COMMAND ----------
+
+

c9ef4387a309c7506d54231ec10e079537c63b57
Aaron Wiegel <aawiegel@gmail.com>
2025-10-07 16:08:00 -0700

    fix test

--- tests/labforge/test_temporal.py
+++ tests/labforge/test_temporal.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def test_forge_date_pairs(np_number_generator):
     received_dates, processed_dates = temporal.forge_date_pairs(np_number_generator)
-    assert processed_dates > received_dates
+    assert (processed_dates > received_dates).all()
     assert np.strings.startswith(received_dates, "2024").all()
     assert received_dates.dtype.kind == "U"
     assert processed_dates.dtype.kind == "U"

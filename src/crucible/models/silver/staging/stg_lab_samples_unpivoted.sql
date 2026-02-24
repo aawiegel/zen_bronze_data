7c5c6c07431b23689a2634395ee70532682bef7c
Aaron Wiegel <aawiegel@gmail.com>
2026-02-24 01:22:35 -0800

    add in staging, intermediate models, clean up metadata

---
+++ src/crucible/models/silver/staging/stg_lab_samples_unpivoted.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='view') }}
+
+WITH lab_samples_unpivoted AS (
+    SELECT * FROM  {{ source('bronze', 'lab_samples_unpivoted') }}
+),
+
+lab_samples_unpivoted_staged AS (
+    SELECT
+        *,
+        LOWER(
+            TRIM(
+                TRANSLATE(
+                    lab_samples_unpivoted.lab_provided_attribute,
+                    '-$()#./ %@!',
+                    '_'
+                )
+            )
+        ) AS attribute_standardized
+    FROM lab_samples_unpivoted
+)
+
+SELECT * FROM lab_samples_unpivoted_staged
